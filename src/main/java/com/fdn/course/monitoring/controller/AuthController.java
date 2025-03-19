d4491e17f81bf30b1f179fc343af25cc24f64a83
AdifFutuh <a764b4ky1564@bangkit.academy>
2025-03-20 00:59:30 +0700

    resend otp, login proses

--- src/main/java/com/fdn/course/monitoring/controller/AuthController.java
+++ src/main/java/com/fdn/course/monitoring/controller/AuthController.java
@@ -1,12 +1,16 @@
 package com.fdn.course.monitoring.controller;
 
+import com.fdn.course.monitoring.dto.validation.ValLoginDTO;
 import com.fdn.course.monitoring.dto.validation.ValRegistrationDTO;
+import com.fdn.course.monitoring.dto.validation.ValVerifyRegisDTO;
 import com.fdn.course.monitoring.service.UserDetailsServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("auth")
@@ -20,9 +24,23 @@ public class AuthController {
         return userDetailsService.regis(userDetailsService.convertDtoToEntity(regisDTO), request);
     }
 
-    @GetMapping
-    public String tes(){
-        return "oke";
+    @PostMapping("/verify-regis")
+    public ResponseEntity<Object> verifyRegis(@Valid @RequestBody ValVerifyRegisDTO verifyRegisDTO,
+                                              HttpServletRequest request){
+        return userDetailsService.verifyRegis(userDetailsService.convertDtoToEntity(verifyRegisDTO),request);
+    }
+
+    @PostMapping("/resend-otp")
+    public ResponseEntity<Object> resendOtp(@RequestBody Map<String, String> requestBody,
+                                            HttpServletRequest request){
+        String email = requestBody.get("email");
+       return userDetailsService.resendOtp(email, request);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody ValLoginDTO valLoginDTO,
+                                              HttpServletRequest request){
+        return userDetailsService.login(userDetailsService.convertDtoToEntity(valLoginDTO),request);
     }
 
 }
--- src/main/java/com/fdn/course/monitoring/service/UserDetailsServiceImpl.java
+++ src/main/java/com/fdn/course/monitoring/service/UserDetailsServiceImpl.java
@@ -1,11 +1,17 @@
 package com.fdn.course.monitoring.service;
 
+import com.fdn.course.monitoring.config.JwtConfig;
+import com.fdn.course.monitoring.dto.validation.ValLoginDTO;
 import com.fdn.course.monitoring.dto.validation.ValRegistrationDTO;
+import com.fdn.course.monitoring.dto.validation.ValVerifyRegisDTO;
+import com.fdn.course.monitoring.handler.GlobalResponse;
 import com.fdn.course.monitoring.model.Otp;
 import com.fdn.course.monitoring.model.User;
 import com.fdn.course.monitoring.repository.OtpRepository;
 import com.fdn.course.monitoring.repository.UserRepository;
 import com.fdn.course.monitoring.security.BcryptImpl;
+import com.fdn.course.monitoring.security.Crypto;
+import com.fdn.course.monitoring.security.JwtUtility;
 import com.fdn.course.monitoring.util.SendMailOTP;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
@@ -36,6 +42,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private JwtUtility jwtUtility;
 
     private final Random random = new Random();
 
@@ -84,8 +93,115 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return ResponseEntity.ok(map);
     }
 
+    public ResponseEntity<Object> verifyRegis(User user, HttpServletRequest request) {
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+
+        if (userOptional.isEmpty()){
+            return GlobalResponse.dataTidakDitemukan("",request);
+        }
+        User userNext = userOptional.get();
+        Otp otp = userNext.getOtp();
+
+        if (otp == null) {
+            return GlobalResponse.dataTidakValid("", request);
+        }
+
+        if (LocalDateTime.now().isAfter(otp.getExpiredTime())) {
+            return GlobalResponse.dataTidakValid("", request);
+        }
+
+        if (!BcryptImpl.verifyHash(user.getOtp().getOtp(), otp.getOtp())) {
+            return GlobalResponse.dataTidakValid("", request);
+        }
+
+        userNext.setIsRegistered(true);
+        userRepository.save(userNext);
+
+        return GlobalResponse.dataBerhasilDiubah(request);
+    }
+
+    public ResponseEntity<Object> resendOtp(String email, HttpServletRequest request) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isEmpty()) {
+            return GlobalResponse.dataTidakDitemukan("", request);
+        }
+
+        User user = userOptional.get();
+        Otp otp = user.getOtp();
+
+        int newOtp = random.nextInt(111111, 999999);
+        String hashedOtp = BcryptImpl.hash(String.valueOf(newOtp));
+
+        if (otp == null) {
+            otp = new Otp();
+            otp.setUser(user);
+        }
+
+        otp.setOtp(hashedOtp);
+        otp.setExpiredTime(LocalDateTime.now().plusMinutes(5));
+        otpRepository.save(otp);
+
+        SendMailOTP.verifyRegisOTP(
+                "RESEND OTP VERIFIKASI",
+                user.getNama(),
+                user.getEmail(),
+                String.valueOf(newOtp)
+        );
+
+        return GlobalResponse.dataBerhasilDiubah(request);
+    }
+
+
+    public ResponseEntity<Object> login(User user, HttpServletRequest request) {
+        Optional<User> userOptional = userRepository.findByUsername(user.getUsername());
+
+        if (userOptional.isEmpty()){
+            return GlobalResponse.dataTidakDitemukan("",request);
+        }
+        User userNext = userOptional.get();
+
+        if (!userNext.getIsRegistered()){
+            return GlobalResponse.dataTidakDitemukan("",request);
+        }
+
+        if (!BcryptImpl.verifyHash(user.getUsername()+user.getPassword(), userNext.getPassword())){
+            return GlobalResponse.passwordtidakValid("", request);
+        }
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id", userNext.getId());
+        claims.put("phn", userNext.getNoHp());
+        claims.put("nl", userNext.getNama());
+        claims.put("ml", userNext.getEmail());
+
+        String token = jwtUtility.doGenerateToken(claims,user.getUsername());
+        if (JwtConfig.getTokenEncryptEnable().equals("y")){
+            token = Crypto.performEncrypt(token);
+        }
+        Map<String,Object> response = new HashMap<>();
+        response.put("token", token);
+        return GlobalResponse.loginBerhasil(response, request);
+    }
+
+
 
     public User convertDtoToEntity(ValRegistrationDTO regisDTO){
         return modelMapper.map(regisDTO, User.class);
     }
+
+    public User convertDtoToEntity(ValVerifyRegisDTO verifyRegisDTO){
+        User user = new User();
+        Otp otp = new Otp();
+        otp.setOtp(verifyRegisDTO.getOtp());
+        user.setEmail(verifyRegisDTO.getEmail());
+        user.setOtp(otp);
+
+        return user;
+    }
+
+    public User convertDtoToEntity(ValLoginDTO loginDTO){
+        return modelMapper.map(loginDTO, User.class);
+    }
+
 }
